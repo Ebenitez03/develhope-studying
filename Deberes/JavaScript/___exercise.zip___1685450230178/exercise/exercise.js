0506070cf554e30b814619982b87097d130272d3
Ebenitez03 <elianenbenitezd04@gmail.com>
2023-10-18 15:14:11 +0200

    Feat
    Added homework folders
    
    Finished Nav responsive exercise

---
+++ Deberes/JavaScript/___exercise.zip___1685450230178/exercise/exercise.js
@@ -0,0 +1,20 @@
+
+function calculateSalary(role) {
+  let month= role;
+  let monthDays= 28;
+
+  let salary= 2200/28;
+  return salary
+}
+
+const ceoSalary = calculateSalary('ceo');
+const managerSalary = calculateSalary('manager');
+const ctoSalary = calculateSalary('cto');
+const developerSalary = calculateSalary('developer');
+const otherSalary = calculateSalary('other');
+
+console.log(ceoSalary);
+console.log(managerSalary);
+console.log(ctoSalary);
+console.log(developerSalary);
+console.log(otherSalary);
