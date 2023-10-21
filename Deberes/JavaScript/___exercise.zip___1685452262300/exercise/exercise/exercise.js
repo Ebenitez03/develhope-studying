542603ca3910b5745eb179ee704737fb6616a082
Ebenitez03 <elianenbenitezd04@gmail.com>
2023-10-21 21:35:48 +0200

    Feat
    Finished Object copying exercise

--- Deberes/JavaScript/___exercise.zip___1685452262300/exercise/exercise/exercise.js
+++ Deberes/JavaScript/___exercise.zip___1685452262300/exercise/exercise/exercise.js
@@ -6,7 +6,40 @@ const person1 = {
 
 const person2 = person1;
 
+
 // Modify the property `firstName` of the `person2` in "Simon".
 
 console.log(person1);
 console.log(person2);
+
+
+function changeVals(person2){
+  person2.firstName="Simon";
+}
+
+changeVals(person2);
+
+console.log(person1);
+console.log(person2);
+
+/*
+I'll explain it with A=Person1 B=Person2 example.
+
+If we say A=5 , A will storage 5 as it's value.
+
+If then we say B=A, B will storage A as it's value,
+therefore, B will be B=5;
+
+But now if we change B, who is keeping A inside it,
+A will be equal to A=B.
+
+So, if we now do B=10; A will turn into A=10,
+because it's storaged in B.
+
+This will be the logical path:
+        A=5;
+        B=A => B=5;
+        B=10=> B=A=10;
+
+They are connected in the order they are written.
+*/
