06e748b8b857b878846a2070cd23deac1c4e17f2
Ebenitez03 <elianenbenitezd04@gmail.com>
2023-10-29 04:57:49 +0100

    Feat
    Finished sumUntil exercise
    
    Commited to correction

--- Deberes/JavaScript/___exercise.zip___1685452150413/exercise/exercise.js
+++ Deberes/JavaScript/___exercise.zip___1685452150413/exercise/exercise.js
@@ -1,5 +1,10 @@
 function sumUntil(maxValue) {
-  // ...
+  let sum;
+  for (let index = 0; index<=maxValue; index++){
+    sum= index+index;
+    sum=sum+index;
+  }
+  return sum;
 }
 
 console.log(sumUntil(5));
