654bab977e34763344c4ce29fbb4e367af9120b9
Ebenitez03 <elianenbenitezd04@gmail.com>
2023-10-29 04:31:56 +0100

    Feat
    Finished operation exercise
    
    Saved for correction
    
    Keep working on it later

--- Deberes/JavaScript/___exercise.zip___1685452022674/exercise/exercise.js
+++ Deberes/JavaScript/___exercise.zip___1685452022674/exercise/exercise.js
@@ -1,18 +1,17 @@
 
-const sum = (a, b)=> {
-   a + b;
+let sum=(a,b)=> {
+   return a+b;
+}
+let subtract =  (a, b) =>{
+   return a - b;
 }
 
-const subtract =  (a, b) =>{
-   a - b;
+let multiply =  (a, b)=> {
+   return a * b;
 }
 
-const multiply =  (a, b)=> {
-  a * b;
-}
-
-const divide= (a, b)=> {
-   a / b;
+let divide= (a, b)=> {
+   return a / b;
 }
 
 const log = (value)=> {
@@ -21,4 +20,25 @@ const log = (value)=> {
 
 console.log("((2+4)*(2+5)-2)/5");
 
-console.log(sum(2,4));
+function calc(sum, subtract,multiply,divide) {
+   let a= sum(2,4);
+   let b= sum(5,2);
+   let c= multiply(a,b);
+   let d= subtract(c,2);
+   let e= divide(d,5);
+   
+   log(e);
+   
+   log(divide(subtract(multiply(sum(2,4),sum(5,2)),2),5));
+}
+
+calc(sum, subtract,multiply,divide);
+
+/*
+function calculator() {
+
+   let operation= prompt("Introduce tu operacion:");
+}
+
+calculator();
+*/
