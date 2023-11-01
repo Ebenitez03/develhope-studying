875506eecba60a4b3185a09ff1807272a6e1dc4f
Ebenitez03 <elianenbenitezd04@gmail.com>
2023-11-01 09:18:57 +0100

    Feat Docs
    Added Rest parameters and spread sintax homework folders
    
    Finished first exercise

---
+++ Deberes/JavaScript/Rest parameters and spread syntax/Rest parameters and spread syntax/exercise/exercise.js
@@ -0,0 +1,10 @@
+// let numberStore = [0, 1, 2];
+// let newNumber = 3;
+// numberStore.push(newNumber);
+// console.log(numberStore);
+
+let numberStore=[0,1,2];
+let newNumber=3;
+
+let newNumberStore=[...numberStore,newNumber];
+console.log(newNumberStore);
